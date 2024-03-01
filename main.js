d0ebf6d9ecb581d0ae3c8e207be831d34f4c2d07
Edgar Baudry <edgar.baudry@gmail.com>
2024-02-29 17:27:59 -0800

    ll

--- main.js
+++ main.js
@@ -65,21 +65,10 @@ const Part_one_hermite_base = defs.Part_one_hermite_base =
         this.shapes = { 'box'  : new defs.Cube(),
           'ball' : new defs.Subdivision_Sphere( 4 ),
           'axis' : new defs.Axis_Arrows(),
-          'teapot': new Shape_From_File( "assets/teapot.obj" ),
-          'ship': new Shape_From_File( "assets/ship.obj" ),
         };
 
         this.vertical_input = 0;
         this.horizontal_input = 0;
-
-        const background_top = vec4(0.8, 0.9, 1.0, 1); // Light blue
-        const background_bottom = vec4(0.2, 0.3, 0.4, 1); // Dark blue
-        const fogParams = {
-          color: vec4(0.5, 0.6, 0.7, 1), // Fog color
-          min_distance: 20,
-          max_distance: 100,
-          intensity: 0.5
-        };
 
         const phong = new defs.Phong_Shader(1);
         const tex_phong = new defs.Textured_Phong(1);
@@ -94,34 +83,6 @@ const Part_one_hermite_base = defs.Part_one_hermite_base =
         this.materials.ocean = { shader: phong, ambient: .3, diffusivity: 1, specularity: .5, color: color( 0,0.62,0.77,1 ) }
         this.materials.oceanfloor = { shader: phong, ambient: .3, diffusivity: 1, specularity: .5, color: color( 0.5,0.5,0.5,1 ) }
         
-
-        // call this with side_length = density+1
-        const get_corners = (side_length) => [0, side_length-1, side_length**2-side_length, side_length**2-1];
-        const get_edge = (side_length) => Array.from({length: side_length}, (_, i) => i);
-
-        const sailConfig = {
-          initPos : vec3(0,4,0),
-          density : 10,
-          size : 4,
-          lockedPoints: get_corners(11),
-          material: this.materials.cloth_tex,
-        }
-        const sailConfig2 = {
-          initPos : vec3(0,3.25,2.75),
-          density : 10,
-          size : 2,
-          lockedPoints: get_corners(11),
-          material: this.materials.cloth_tex,
-        }
-
-        const flagConfig = {
-          initPos : vec3(1,8,-.2),
-          density : 10,
-          size : 2,
-          lockedPoints: get_edge(11),
-          material: this.materials.flag_tex
-        }
-
         const oceanConfig = {
           initPos : vec3(0,0,0),
           density : 40,
@@ -134,30 +95,9 @@ const Part_one_hermite_base = defs.Part_one_hermite_base =
           wave_amplitude: 0.5
         }
 
-        const ocean2Config = {
-          initPos : vec3(0,-0.5,0),
-          density : 40,
-          size : 200,
-          material: this.materials.ocean,
-          floorDensity : 10,
-          floorMinY : -10,
-          floorMaxY : -9,
-          floorMaterial: this.materials.oceanfloor,
-          wave_amplitude: 0.5
-        }
-
-
-
-        this.sail = new Cloth(sailConfig)
-        this.sail2 = new Cloth(sailConfig2)
-        this.flag = new Cloth(flagConfig)
-
         this.ocean = new Ocean(oceanConfig)
-        // this.ocean2 = new Ocean(ocean2Config)
 
         this.ship = new Ship()
-
-        this.rb = new RigidBody(vec3(0, 4, 0), vec3(0, 0, 0), 2000, vec3(1,1,1), vec4(0, 0, 1, 0), 100);
 
       }
 
@@ -239,9 +179,6 @@ const Part_one_hermite_base = defs.Part_one_hermite_base =
         // !!! Light changed here
         const light_position = vec4(20, 20, -10, 1.0);
         this.uniforms.lights = [ defs.Phong_Shader.light_source( light_position, color( 1,1,1,1 ), 1000000 ) ];
-
-        // draw axis arrows.
-        // this.shapes.axis.draw(caller, this.uniforms, Mat4.identity().times(Mat4.scale(0.5,0.5,0.5)), this.materials.metal);
       }
     }
 
@@ -254,42 +191,13 @@ export class Part_one_hermite extends Part_one_hermite_base
     super.render_animation( caller );
     const sea_blue = color( 0,0.62,0.77,1 ), whiteish = color( .9,.9,1,1 ), brown = color(139/255, 69/255, 19/255,1);
     const t = this.t = this.uniforms.animation_time/1000;
-
-    // !!! Draw ground
-    // let floor_transform = Mat4.translation(0, 0, 0).times(Mat4.scale(10, 0.01, 10));
-    // this.shapes.box.draw( caller, this.uniforms, floor_transform, { ...this.materials.shiny, color: sea_blue } );
-
-    // TODO: you should draw
-    // this.sail.simulate(this.t, this.dt)
-    // this.sail.show(this.shapes, caller, this.uniforms);
-
-    // this.flag.simulate(this.t, this.dt)
-    // this.flag.show(this.shapes, caller, this.uniforms);
-
-    // this.sail2.simulate(this.t, this.dt)
-    // this.sail2.show(this.shapes, caller, this.uniforms)
-
-    // this.shapes.ship.draw( caller, this.uniforms, Mat4.translation(0, 1.5, .5).times(Mat4.scale(2.3,2.3,2.3)), this.materials.wood );
-
     this.ocean.simulate(this.t, this.dt)
     this.ocean.show(this.shapes, caller, this.uniforms)
 
     this.ship.update(this.t, this.dt)
     this.ship.show(caller, this.uniforms)
 
-    this.ocean.applyWaterForceOnRigidBody(this.ship.rb, this.dt)
-
-    // this.ocean2.simulate(this.t, this.dt)
-    // this.ocean2.show(this.shapes, caller, this.uniforms)
-  
-    // this.rb.applyForceAtPosition(vec3(0,20,0).times(this.vertical_input), vec3(1,0,1))
-    // this.rb.applyForce(vec3(0,-gravity * this.rb.mass, 0))
-    // this.rb.update(this.dt)
-    // this.rb.show(caller, this.uniforms)
-
-    // let temp_pos = vec3(0,1.48,0)
-    // let tt = Mat4.translation(temp_pos[0], temp_pos[1], temp_pos[2]).times(Mat4.scale(.1, .1, .1));
-    // this.shapes.ball.draw( caller, this.uniforms, tt, { ...this.materials.shiny, color: whiteish } );
+    this.ocean.applyWaterForceOnRigidBody(this.ship.rb, this.dt);
   }
 
 
