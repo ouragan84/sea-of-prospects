98f75f78c75ab2d96737aa18530fd1e6a684df87
Edgar Baudry <edgar.baudry@gmail.com>
2024-03-04 17:51:03 -0800

    script edit

--- gerstner-gen.py
+++ gerstner-gen.py
@@ -1,15 +1,19 @@
+# ALTER PARAMETERS HERE
+# PASTE OUTPUT INTO Ocean.js, line 25
+# can do `python gerstner-gen.py | pbcopy` to copy directly to clipboard
+
 n = 10
-max_steepness = 0.8
+max_steepness = 0.5
 min_steepness = 0.01
 max_length = 20.0
 min_length = 0.1
-min_vel = 0.1
-max_vel = 10.0
+min_vel = 1.0
+max_vel = 20.0
 
 import random
 import math
 
-def inv_exp_rand( common, rare, random_deviation, random_center ):
+def inv_exp_rand( rare, common, random_deviation, random_center ):
     random_val = (random.random()*2-1) * random_deviation + random_center
     val = math.pow( common / rare, random_val ) * rare
     if common < rare and val < common or common > rare and val > common:
@@ -18,46 +22,29 @@ def inv_exp_rand( common, rare, random_deviation, random_center ):
         val = rare
     return val
 
-for i in range(n):
-    # create linspace from 0 to 1 with n points=
-    rand_centers = [(i+1)/n for i in range(n)]
-    rand_deviation = .5/n
+# create linspace from 0 to 1 with n points=
+rand_centers = [(n-i)/n for i in range(n)]
+rand_deviation = .5/n
 
-    waves = []
+waves = []
 
-    for center in rand_centers:
-        steepness = inv_exp_rand(min_steepness, max_steepness, rand_deviation, center)
-        length = inv_exp_rand(min_length, max_length, rand_deviation, center)
-        vel = inv_exp_rand(max_vel, min_vel, rand_deviation, center)
-        dirx = random.random() * 2 - 1
-        diry = random.random() * 2 - 1
-        waves.append([steepness, length, vel, dirx, diry])
+for center in rand_centers:
+    steepness = inv_exp_rand(min_steepness, max_steepness, rand_deviation, center)
+    length = inv_exp_rand(min_length, max_length, rand_deviation, center)
+    vel = inv_exp_rand(max_vel, min_vel, rand_deviation, center)
+    dirx = random.random() * 2 - 1
+    diry = random.random() * 2 - 1
+    waves.append([steepness, length, vel, dirx, diry])
 
-    '''
-    Generate code in format:
-    this.n = 5;
-    this.s = [.5, 0.4, 0.2, 0.13, 0.1];  // steepness
-    this.l = [4.0, 3.0, 1.0, 0.8, 0.5];  // wave length
-    this.v = [1.0, 1.5, 2.0, 2.5, 3.0];  // velocity
+str_out = ""
 
-    this.dir = [vec3(1, 0, -.4),
-                vec3(.2, 0, .9),
-                vec3(-.6, 0, .4),
-                vec3(-.8, 0, -.5),
-                vec3(.3, 0, -.8)
-            ];  // direction vector
-    '''
+str_out += "this.n = " + str(n) + ";\n"
+str_out += "this.s = [" + ", ".join([str(w[0]) for w in waves]) + "];\n"
+str_out += "this.l = [" + ", ".join([str(w[1]) for w in waves]) + "];\n"
+str_out += "this.v = [" + ", ".join([str(w[2]) for w in waves]) + "];\n"
+str_out += "this.dir = ["
+for w in waves:
+    str_out += "vec3(" + str(w[3]) + ", 0, " + str(w[4]) + "),\n"
+str_out += "];"
 
-    str_out = ""
-
-    str_out += "this.n = " + str(n) + ";\n"
-    str_out += "this.s = [" + ", ".join([str(w[0]) for w in waves]) + "];\n"
-    str_out += "this.l = [" + ", ".join([str(w[1]) for w in waves]) + "];\n"
-    str_out += "this.v = [" + ", ".join([str(w[2]) for w in waves]) + "];\n"
-    str_out += "this.dir = ["
-    for w in waves:
-        str_out += "vec3(" + str(w[3]) + ", 0, " + str(w[4]) + "),\n"
-    str_out += "];\n"
-
-    print(str_out)
-    print("\n")
+print(str_out)
